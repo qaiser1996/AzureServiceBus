164d34c721aa2bc1d0b70824354dca6a82c130c7
qaiser1996 <qaiserh30@gmail.com>
2020-08-11 15:30:30 +0500

    First Commit

---
+++ AzureServiceBus-Receiver/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace AzureServiceBus_Receiver
+{
+    class Program
+    {
+        private static IQueueClient serviceClient;
+        private const string connection_string = "Endpoint=sb://systems.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=g02W1R8Vwd435GKxEX3313qIU/xsYrjqHhySs/HhYf0=";
+        private const string queue_name = "test_queue";
+
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync()
+        {
+            serviceClient = new QueueClient(connection_string, queue_name);
+            RegisterOnMessageHandlerAndReceiveMessages();
+            Console.ReadKey();
+            await serviceClient.CloseAsync();
+        }
+
+        private static void RegisterOnMessageHandlerAndReceiveMessages()
+        {
+            MessageHandlerOptions messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler);
+            messageHandlerOptions.MaxConcurrentCalls = 1;
+            messageHandlerOptions.AutoComplete = false;
+            
+
+            serviceClient.RegisterMessageHandler(ProcessMessageAsync, messageHandlerOptions);
+        }
+
+        private static async Task ProcessMessageAsync(Message message, CancellationToken token)
+        {
+            Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+            await serviceClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        private static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {context.Endpoint}");
+            Console.WriteLine($"- Entity Path: {context.EntityPath}");
+            Console.WriteLine($"- Executing Action: {context.Action}");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ AzureServiceBus/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace AzureServiceBus
+{
+    class Program
+    {
+
+        private static IQueueClient serviceClient;
+        private const string connection_string = "Endpoint=sb://systems.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=g02W1R8Vwd435GKxEX3313qIU/xsYrjqHhySs/HhYf0=";
+        private const string queue_name = "test_queue";
+
+        public static async Task Main(string[] args)
+        {
+            serviceClient = new QueueClient(connection_string, queue_name);
+            while (true)
+            {
+                Console.WriteLine("Enter a Message or -1 to Exit Application.");
+
+                string message = Console.ReadLine();
+
+                if (!message.Equals("-1"))
+                {
+                    await SendAsyncMessage(PutToJSON(message));
+                }
+                else
+                {
+                    Console.WriteLine("Exiting");
+                    await serviceClient.CloseAsync();
+                    break;
+                }
+            }
+            
+        }
+
+        private static async Task SendAsyncMessage(string JSON_message)
+        {
+            try
+            {
+                Message encoded_message = new Message(Encoding.UTF8.GetBytes(JSON_message));
+                Console.WriteLine($"Sending Message: {JSON_message}");
+                await serviceClient.SendAsync(encoded_message);
+            }
+            catch (Exception exception)
+            {
+                throw;
+            }
+        }
+
+        private static string PutToJSON(string message)
+        {
+            return $"{{ message: \"{message}\" }}";
+        }
+    }
+}
