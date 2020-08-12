ca3b31c7d8b03f104da3cfdf5332524fd5ba5327
qaiser1996 <qaiserh30@gmail.com>
2020-08-12 16:05:37 +0500

    Added Topic/Subscriber Test Code

---
+++ AzureServiceBus-SubscriberReceiver/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace AzureServiceBus_SubscriberReceiver
+{
+    class Program
+    {
+
+        private static ISubscriptionClient subscriptionClient;
+        private const string connection_string = "<Connection String>";
+        private const string topic_name = "<Topic>";
+        private const string subscriber_name = "<Subscription Name>";
+
+        static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            subscriptionClient = new SubscriptionClient(connection_string, topic_name, subscriber_name);
+
+            RegisterMessageHandlerOptions();
+
+            Console.ReadKey();
+
+            await subscriptionClient.CloseAsync();
+
+        }
+
+        private static void RegisterMessageHandlerOptions()
+        {
+            MessageHandlerOptions messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+
+            subscriptionClient.RegisterMessageHandler(ProcessMessageAsync, messageHandlerOptions);
+        }
+
+        private static async Task ProcessMessageAsync(Message message, CancellationToken token)
+        {
+            Console.WriteLine($"Message: {Encoding.UTF8.GetString(message.Body)}");
+            await subscriptionClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        private static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {context.Endpoint}");
+            Console.WriteLine($"- Entity Path: {context.EntityPath}");
+            Console.WriteLine($"- Executing Action: {context.Action}");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ AzureServiceBus-TopicSender/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace AzureServiceBus_TopicSender
+{
+    class Program
+    {
+        private const string connection_string = "<Connection String>";
+        private const string topic_name = "<Topic>";
+        private static ITopicClient topicClient;
+
+        static async Task Main(string[] args)
+        {
+            topicClient = new TopicClient(connection_string, topic_name);
+
+            while (true)
+            {
+                Console.WriteLine("Enter a Message or -1 to Exit Application.");
+
+                string message = Console.ReadLine();
+
+                if (!message.Equals("-1"))
+                {
+                    await SendAsyncMessage(PutToJSON(message));
+                }
+                else
+                {
+                    Console.WriteLine("Exiting");
+                    await topicClient.CloseAsync();
+                    break;
+                }
+            }
+        }
+
+        private static async Task SendAsyncMessage(string JSONmessage)
+        {
+            try
+            {
+                Message message = new Message(Encoding.UTF8.GetBytes(JSONmessage));
+                Console.WriteLine($"Sending Message: {JSONmessage}");
+                await topicClient.SendAsync(message);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+
+        private static string PutToJSON(string message)
+        {
+            return $"{{ message: \"{message}\" }}";
+        }
+    }
+}
